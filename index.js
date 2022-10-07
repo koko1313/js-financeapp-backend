446dfec1ce894ce4a7456ab1bed3de488e000082
Kaloyan Velichkov <kaloyan.velichkov.ext@urban-mobility.io>
2022-10-07 15:18:57 +0300

    add test ednpoint

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Hi there!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 })
