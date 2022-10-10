05eed5b4c18139d668e31ba20bfaf59626bef813
Kaloyan Velichkov <kaloyanvellichkov@gmail.com>
2022-10-10 16:16:21 +0300

    example endpoint for insert database entry

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import dbClient from './database/dbClient.js';
+import { v4 as uuid } from 'uuid';
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 await dbClient.connect();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/test', async (req, res) => {
+app.get('/getEntries', async (req, res) => {
     const result = await dbClient.query('SELECT * from test_table'); 
     res.send(result.rows);
+});
+
+app.post('/insertEntry', async (req, res) => {
+    const id = uuid();
+    const name = req.body.name;
+    await dbClient.query(`INSERT INTO test_table (id, name) VALUES ('${id}', '${name}')`); 
+    res.status(200).send();
 });
 
 app.listen(port, () => {
