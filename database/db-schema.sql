eeada2ae45c035b76bd7a43e56c69d970f56e14a
Kaloyan Velichkov <kaloyanvellichkov@gmail.com>
2022-10-14 15:34:09 +0300

    Create database schema

---
+++ database/db-schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "user" (
+    "id" uuid NOT NULL PRIMARY KEY,
+    "email" text,
+    "name" text,
+    "password" text
+);
+
+CREATE TABLE "income" (
+    "id" uuid NOT NULL PRIMARY KEY,
+    "dateTime" text,
+    "amount" numeric,
+    "comment" text,
+    "user_id" uuid,
+    CONSTRAINT income_user
+        FOREIGN KEY(user_id) 
+        REFERENCES "user"(id)
+);
+
+CREATE TABLE "expense" (
+    "id" uuid NOT NULL PRIMARY KEY,
+    "dateTime" text,
+    "amount" numeric,
+    "comment" text,
+    "user_id" uuid,
+    CONSTRAINT income_user
+        FOREIGN KEY(user_id) 
+        REFERENCES "user"(id)
+);
