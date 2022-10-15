f5329524a470a66d625462f9e221ea3a390bcd47
koko1313 <kaloyanvellichkov@gmail.com>
2022-10-15 20:08:58 +0300

    Expense get/add and register dummy user endpoints

--- index.js
+++ index.js
@@ -11,15 +11,28 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/getEntries', async (req, res) => {
-    const result = await dbClient.query('SELECT * from test_table'); 
+app.get('/expense/get', async (req, res) => {
+    const result = await dbClient.query('SELECT * from expense'); 
     res.send(result.rows);
 });
 
-app.post('/insertEntry', async (req, res) => {
+app.post('/expense/add', async (req, res) => {
     const id = uuid();
-    const name = req.body.name;
-    await dbClient.query(`INSERT INTO test_table (id, name) VALUES ('${id}', '${name}')`); 
+    const userId = req.body.userId;
+    const date = req.body.date;
+    const amount = req.body.amount;
+    const comment = req.body.comment;
+    await dbClient.query(`INSERT INTO expense (id, user_id, date, amount, comment) VALUES ('${id}', '${userId}', '${date}', ${amount}, '${comment}')`);
+    res.status(200).send();
+});
+
+// register dummy user
+app.post('/user/register', async (req, res) => {
+    const id = 'ca7c5d1a-b06b-419e-bf5c-0981ea53bff4';
+    const email = 'dummy@mail.com';
+    const name = 'Dummy User'
+    const password = '1234';
+    await dbClient.query(`INSERT INTO "user" (id, email, name, password) VALUES ('${id}', '${email}', '${name}', '${password}')`); 
     res.status(200).send();
 });
 
