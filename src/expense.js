064bc6eecc3c1826d73db337d1d3c64db9bded06
koko1313 <kaloyanvellichkov@gmail.com>
2022-10-15 21:44:32 +0300

    parse expense amount

--- src/expense.js
+++ src/expense.js
@@ -1,8 +1,18 @@
 import { v4 as uuid } from 'uuid';
 
 export const getExpense = async (dbClient) => {
-    const result = await dbClient.query('SELECT * from expense'); 
-    return result.rows;
+    const result = await dbClient.query('SELECT * from expense');
+
+    const expenses = result.rows.map(expense => {
+        return {
+            userId: expense.userId,
+            date: expense.date,
+            amount: parseFloat(expense.amount),
+            comment: expense.comment,
+        };
+    });
+
+    return expenses;
 }
 
 export const addExpense = async (dbClient, expense) => {
