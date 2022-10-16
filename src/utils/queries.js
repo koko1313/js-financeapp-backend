f14e8d54f14ce7a8fb74b86f949632308c53a6c8
koko1313 <kaloyanvellichkov@gmail.com>
2022-10-16 09:31:37 +0300

    Order expenses

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,4 +1,4 @@
 export const QUERIES = {
-    getExpenses: 'SELECT * from expense WHERE user_id = :userId',
+    getExpenses: 'SELECT * from expense WHERE user_id = :userId ORDER BY date DESC',
     addExpense: 'INSERT INTO expense (id, user_id, date, amount, comment) VALUES (:id, :userId, :date, :amount, :comment)',
 };
